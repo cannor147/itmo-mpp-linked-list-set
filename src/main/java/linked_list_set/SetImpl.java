6da69c9c195696e969185acdd940ce2488cfcbac
cannor147 <cannor147@gmail.com>
2020-01-11 17:48:21 +0300

    Implemented concurrent set based on linked list

--- src/main/java/linked_list_set/SetImpl.java
+++ src/main/java/linked_list_set/SetImpl.java
@@ -1,61 +1,25 @@
 package linked_list_set;
 
-import java.util.concurrent.atomic.AtomicMarkableReference;
+import kotlinx.atomicfu.AtomicRef;
 
 public class SetImpl implements Set {
+    private final AtomicRef<Node> head;
 
-    private static class Node {
-        private AtomicMarkableReference<Node> next;
-        private int x;
-
-        private Node(int x, Node next, boolean removed) {
-            this.next = new AtomicMarkableReference<>(next, removed);
-            this.x = x;
-        }
-    }
-
-    private static class Window {
-        Node cur;
-        Node next;
-    }
-
-    private final Node head = new Node(Integer.MIN_VALUE, new Node(Integer.MAX_VALUE, null, false), false);
-
-    private Window findWindows(int x) {
-        while (true) {
-            Window window = new Window();
-            window.cur = head;
-            window.next = window.cur.next.getReference();
-            while (window.next.x < x) {
-                AtomicMarkableReference<Node> s = window.next.next;
-                if (s.isMarked()) {
-                    if (!window.cur.next.compareAndSet(window.next, s.getReference(), false, false)) {
-                        continue;
-                    }
-                    window.next = s.getReference();
-                } else {
-                    window.cur = window.next;
-                    window.next = window.cur.next.getReference();
-                }
-            }
-            if (!window.next.next.isMarked()) {
-                return window;
-            } else {
-                AtomicMarkableReference<Node> s = window.next.next;
-                window.cur.next.compareAndSet(window.next, s.getReference(), true, s.isMarked());
-            }
-        }
+    public SetImpl() {
+        Node maximumNode = new Node(Integer.MAX_VALUE, null);
+        Node minimumNode = new Node(Integer.MIN_VALUE, maximumNode);
+        this.head = new AtomicRef<>(minimumNode);
     }
 
     @Override
     public boolean add(int x) {
         while (true) {
-            Window window = findWindows(x);
+            Window window = findWindow(x);
             if (window.next.x == x) {
                 return false;
             }
-            Node node = new Node(x, window.next, false);
-            if (window.cur.next.compareAndSet(window.next, node, false, false)) {
+            Node node = new Node(x, window.next);
+            if (window.current.next.compareAndSet(window.next, node)) {
                 return true;
             }
         }
@@ -64,13 +28,12 @@ public class SetImpl implements Set {
     @Override
     public boolean remove(int x) {
         while (true) {
-            Window window = findWindows(x);
-            if (window.next.x != x) {
+            Window window = findWindow(x + 1);
+            if (window.current.x != x) {
                 return false;
             }
-            AtomicMarkableReference<Node> s = window.next.next;
-            if (window.next.next.compareAndSet(s.getReference(), s.getReference(), false, true)) {
-                window.cur.next.compareAndSet(window.next, s.getReference(), false, false);
+            Node node = new RemovedNode(window.next);
+            if (window.current.next.compareAndSet(window.next, node)) {
                 return true;
             }
         }
@@ -78,7 +41,62 @@ public class SetImpl implements Set {
 
     @Override
     public boolean contains(int x) {
-        Window window = findWindows(x);
+        Window window = findWindow(x);
         return window.next.x == x;
+    }
+
+    private Window findWindow(int x) {
+        while (true) {
+            Window window = new Window();
+            window.current = head.getValue();
+            window.next = window.current.next.getValue();
+
+            while (window.next.x < x || window.next.removed()) {
+                if (window.current.removed()) {
+                    break;
+                }
+                if (window.next.removed()) {
+                    Node node = window.next.next.getValue().next.getValue();
+                    if (!window.current.next.compareAndSet(window.next, node)) {
+                        window.next = window.current.next.getValue();
+                        continue;
+                    }
+                    window.next = node;
+                } else {
+                    window.current = window.next;
+                    window.next = window.current.next.getValue();
+                }
+            }
+
+            if (window.current.removed()) {
+                continue;
+            }
+            return window;
+        }
+    }
+
+    private static class Window {
+        private Node current;
+        private Node next;
+    }
+
+    private static class Node {
+        protected AtomicRef<Node> next;
+        private int x;
+
+        private Node(int x, Node next) {
+            this.next = new AtomicRef<>(next);
+            this.x = x;
+        }
+
+        public boolean removed() {
+            return next.getValue() instanceof RemovedNode;
+        }
+    }
+
+    private static class RemovedNode extends Node {
+        private RemovedNode(Node next) {
+            super(Integer.MIN_VALUE, next);
+        }
     }
 }
